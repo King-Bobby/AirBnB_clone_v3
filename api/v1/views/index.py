f0b9ac488e34f1ae226b8e47fa932a5d82a7ba33
King-Bobby <noelbrowndboss@gmail.com>
2023-07-31 13:10:27 -0700

    updating index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python3
 """
-Contains Module index
+This contains the Module index
 """
 
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
-def status:
+def status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods['GET'])
+def numofobjects_by_type():
+    classes = {
+            "Amenity": storage.count(Amenity),
+            "Cities": storage.count(City),
+            "Places": storage.count(Places),
+            "Reviews": storage.count(Reviews),
+            "States": storage.count(State),
+            "Users": storage.count(User)
+        }
+    return jsonify(classes)
