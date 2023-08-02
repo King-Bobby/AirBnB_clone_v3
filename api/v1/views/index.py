12e5916c786a1dc89b4d36bd9da2c6aa39a45dd0
King-Bobby <noelbrowndboss@gmail.com>
2023-08-02 11:46:34 -0700

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -17,11 +17,11 @@ def status():
 @app_views.route('/stats', methods=['GET'])
 def numofobjects_by_type():
     classes = {
-            "Amenity": storage.count(Amenity),
-            "Cities": storage.count(City),
-            "Places": storage.count(Place),
-            "Reviews": storage.count(Review),
-            "States": storage.count(State),
-            "Users": storage.count(User)
+            "amenities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User)
         }
     return jsonify(classes)
